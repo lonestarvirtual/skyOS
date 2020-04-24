65e0b910a680ec4278eb91a014d4115e12456522
Jonathan P. Voss <jvoss@onvox.net>
2020-04-23 19:45:47 -0500

    added duration/distance searching to schedule search

--- app/helpers/flights_helper.rb
+++ app/helpers/flights_helper.rb
@@ -1,4 +1,17 @@
 # frozen_string_literal: true
 
 module FlightsHelper
+  # Returns a unique collection of Airport objects
+  # that are scheduled destinations
+  #
+  def unique_destinations
+    Airport.joins(:flight_destinations).uniq
+  end
+
+  # Returns a unique collection of Airport objects
+  # that are scheduled origins
+  #
+  def unique_origins
+    Airport.joins(:flight_origins).uniq
+  end
 end
