7fcc708e67005d440079b8252129a323d5f1417b
Jonathan P. Voss <jvoss@onvox.net>
2020-04-25 22:41:16 -0500

    resolved roster spacing and linted

--- config/initializers/google_analytics.rb
+++ config/initializers/google_analytics.rb
@@ -2,8 +2,8 @@
 
 module GoogleAnalytics
   TRACKING_ID = Rails.application.credentials.dig(
-      :google_analytics,
-      :tracking_id
+    :google_analytics,
+    :tracking_id
   )
 
   JS_URL = "https://www.googletagmanager.com/gtag/js?id=#{TRACKING_ID}"
