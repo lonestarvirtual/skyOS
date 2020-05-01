7f569ee5667080f478d2c79a786fa13a64a44ddf
Jonathan P. Voss <jvoss@onvox.net>
2020-05-01 13:15:25 -0500

    removed unneded profile helper spec

--- spec/helpers/profiles_helper_spec.rb
+++ spec/helpers/profiles_helper_spec.rb
@@ -12,6 +12,6 @@ require 'rails_helper'
 #     end
 #   end
 # end
-RSpec.describe ProfilesHelper, type: :helper do
-  pending "add some examples to (or delete) #{__FILE__}"
-end
+# RSpec.describe ProfilesHelper, type: :helper do
+#   pending "add some examples to (or delete) #{__FILE__}"
+# end
