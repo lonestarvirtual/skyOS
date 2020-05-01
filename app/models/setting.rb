5bf4477e4d290bfd691f5931e9db0920b44312a5
Jonathan P. Voss <jvoss@onvox.net>
2020-05-01 17:12:12 -0500

    updated default discord link

--- app/models/setting.rb
+++ app/models/setting.rb
@@ -4,7 +4,7 @@
 class Setting < RailsSettings::Base
   field :admin_emails, default: 'admin@example.com', type: :array
   field :allow_signup, { default: 1, type: :boolean }
-  field :discord_link, { default: 'https://discord.gg/BYMJeyM' }
+  field :discord_link, { default: 'https://discord.gg/BjTXJPv' }
   field :organization_icao, { default: 'LSX' }
   field :recaptcha_min_score, { default: 0.5, type: :float }
 end
