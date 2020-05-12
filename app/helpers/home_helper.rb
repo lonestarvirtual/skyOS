332bc9e2a71bc0ac43cd67d46e500fd1be24dbb7
Jonathan P. Voss <jvoss@onvox.net>
2020-05-12 09:35:06 -0500

    updated home page

--- app/helpers/home_helper.rb
+++ app/helpers/home_helper.rb
@@ -1,4 +1,12 @@
 # frozen_string_literal: true
 
 module HomeHelper
+  def membership_statistics
+    stats = {}
+    stats[:routes]        = Flight.all.count
+    stats[:pilots]        = Pilot.where(active: true).count
+    stats[:total_flights] = Pirep.approved.count
+    stats[:total_hours]   = Pirep.approved.sum(:duration)
+    stats
+  end
 end
