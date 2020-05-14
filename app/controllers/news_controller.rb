7a1f42b7ac54770c3b429ab3e0da31c27a132ab5
Carson Page <me@carson.page>
2020-05-14 17:01:18 -0500

    Basic framing of index for news

--- app/controllers/news_controller.rb
+++ app/controllers/news_controller.rb
@@ -1,2 +1,12 @@
+# frozen_string_literal: true
+
 class NewsController < ApplicationController
+  skip_before_action :authenticate_pilot!, only: %i[index]
+  skip_after_action  :verify_authorized, only: %i[index]
+
+  def index
+    authorize Article, :index?
+    @q = policy_scope(Article).ransack(params[:q])
+    @articles = @q.result.page(params[:page])
+  end
 end
