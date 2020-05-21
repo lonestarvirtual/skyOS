838b3a61f6235a33114d73c4bba6b9346f6af9f0
Jonathan P. Voss <jvoss@onvox.net>
2020-05-21 13:39:38 -0500

    updated this channel to only connect if the user is signed in

--- app/javascript/channels/notifications_channel.js
+++ app/javascript/channels/notifications_channel.js
@@ -1,26 +1,31 @@
 import consumer from "./consumer"
 
-consumer.subscriptions.create("NotificationsChannel", {
-  connected() {},
-  disconnected() {},
+// Check meta tag to determine if this channel should attempt to connect
+if ($("meta[name='current-pilot']").length > 0) {
+  consumer.subscriptions.create("NotificationsChannel", {
+    connected() {
+    },
+    disconnected() {
+    },
 
-  received(data) {
-    // Called when there's incoming data on the websocket for this channel
-    // Prepend the list
-    $("#notifications").prepend(data.notification)
-    // Update the counter
-    $('#notification-counter').text(
-      parseInt($('#notification-counter').text()) + 1
-    )
+    received(data) {
+      // Called when there's incoming data on the websocket for this channel
+      // Prepend the list
+      $("#notifications").prepend(data.notification)
+      // Update the counter
+      $('#notification-counter').text(
+          parseInt($('#notification-counter').text()) + 1
+      )
 
-    // Ensure the bell icon shows a waiting notification
-    $('#notification-indicator').show()
+      // Ensure the bell icon shows a waiting notification
+      $('#notification-indicator').show()
 
-    if ($('#notification-container').is(':hidden')) {
-      $('#notification-container').dropdown('toggle')
+      if ($('#notification-container').is(':hidden')) {
+        $('#notification-container').dropdown('toggle')
+      }
+
+      // Play the notification sound
+      $('#notification-bell')[0].play()
     }
-
-    // Play the notification sound
-    $('#notification-bell')[0].play()
-  }
-});
+  });
+}
