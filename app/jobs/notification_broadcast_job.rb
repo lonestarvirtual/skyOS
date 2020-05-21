24ba4136ac6f6683f47f283d597c6446c85e87c5
Jonathan P. Voss <jvoss@onvox.net>
2020-05-21 16:59:08 -0500

    added rendering in Pilot's time zone for async job

--- app/jobs/notification_broadcast_job.rb
+++ app/jobs/notification_broadcast_job.rb
@@ -14,9 +14,12 @@ class NotificationBroadcastJob < ApplicationJob
   private
 
   def render_notification(notification)
-    NotificationsController.render(
-      partial: 'notification',
-      locals: { notification: notification }
-    ).squish
+    # render the notification in the Pilot's timezone
+    Time.use_zone notification.pilot.time_zone do
+      NotificationsController.render(
+        partial: 'notification',
+        locals: { notification: notification }
+      ).squish
+    end
   end
 end
