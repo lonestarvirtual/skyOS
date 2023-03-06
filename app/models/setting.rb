f9c7b96d80470f3b91fb12769b59f592d65bdc5a
Jonathan Voss <jvoss@onvox.net>
2023-03-06 16:09:20 -0600

    update settings

--- app/models/setting.rb
+++ app/models/setting.rb
@@ -3,21 +3,21 @@
 # RailsSettings Model
 class Setting < RailsSettings::Base
   field :admin_emails, default: 'admin@example.com', type: :array
-  field :allow_signup, { default: 1, type: :boolean }
-  field :copyright_name, { default: 'New Virtual Airline' }
-  field :copyright_year, { default: Time.current.year, type: :integer }
-  field :discord_link, { default: '' }
-  field :discord_widget, { default: '' }
-  field :google_analytics, { default: 0, type: :boolean }
-  field :google_analytics_anonymous, { default: 0, type: :boolean }
-  field :google_analytics_id, { default: '' }
-  field :organization_icao, { default: 'ORG' }
-  field :organization_name, { default: 'New Virtual Airline' }
-  field :organization_tag_line, { default: "Today's premier virtual airline." }
-  field :recaptcha_enabled, { default: 0, type: :boolean }
-  field :recaptcha_min_score, { default: 0.5, type: :float }
-  field :recaptcha_secret_key, { default: '' }
-  field :recaptcha_site_key, { default: '' }
-  field :reply_to, { default: 'configure_reply_to_setting@example.com' }
-  field :starting_pid, { default: 100, type: :integer }
+  field :allow_signup, default: 1, type: :boolean
+  field :copyright_name, default: 'New Virtual Airline'
+  field :copyright_year, default: Time.current.year, type: :integer
+  field :discord_link, default: ''
+  field :discord_widget, default: ''
+  field :google_analytics, default: 0, type: :boolean
+  field :google_analytics_anonymous, default: 0, type: :boolean
+  field :google_analytics_id, default: ''
+  field :organization_icao, default: 'ORG'
+  field :organization_name, default: 'New Virtual Airline'
+  field :organization_tag_line, default: "Today's premier virtual airline."
+  field :recaptcha_enabled, default: 0, type: :boolean
+  field :recaptcha_min_score, default: 0.5, type: :float
+  field :recaptcha_secret_key, default: ''
+  field :recaptcha_site_key, default: ''
+  field :reply_to, default: 'configure_reply_to_setting@example.com'
+  field :starting_pid, default: 100, type: :integer
 end
